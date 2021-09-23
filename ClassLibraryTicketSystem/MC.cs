7f578004f278c4a3510291cda3dfea9d8c527c20
RADU EUGEN ADUMITROAIEI <radu0210@edu.easj.dk>
2021-09-23 16:56:17 +0200

    Task3

---
+++ ClassLibraryTicketSystem/MC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassLibraryTicketSystem
+{
+    /// <summary>
+    /// Class representing a MC object with LicensePLate and Date properties
+    /// </summary>
+    public class MC
+    {
+        public string LicensePlate { get; set; }
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// Returns the price for this car
+        /// </summary>
+        /// <returns>240</returns>
+        public double Price()
+        {
+            return 125;
+        }
+        /// <summary>
+        /// Returns the vehicle type
+        /// </summary>
+        /// <returns>Car</returns>
+        public string VehicleType()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ ClassLibraryTicketSystemTests/MCTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            var mc = new MC();
+            var output = mc.Price();
+            Assert.AreEqual(125, output);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            var mc = new MC();
+            var output = mc.VehicleType();
+            Assert.AreEqual("MC", output);
+        }
+    }
+}
