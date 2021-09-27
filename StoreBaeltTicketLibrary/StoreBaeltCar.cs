ca4ce53fcc6bb5929d894f8665d8b4469cf14a38
RADU EUGEN ADUMITROAIEI <radu0210@edu.easj.dk>
2021-09-27 09:33:42 +0200

    Task9Tests

---
+++ StoreBaeltTicketLibrary/StoreBaeltCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ClassLibraryTicketSystem;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StoreBaeltCar:Car
+    {
+        public StoreBaeltCar() { }
+        public StoreBaeltCar(string licenseplate, DateTime date) : base(licenseplate, date) { }
+        public StoreBaeltCar(string licenseplate, DateTime date, bool brobiz) : base(licenseplate, date, brobiz) { }
+
+        public override double Price()
+        {
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                if (Brobizz) return 240 - (240 * 0.2) - ((240-(240 * 0.2))*0.05);
+                return 240 - (240 * 0.2);
+            }
+            if (Brobizz) return 240 - (240 * 0.05);
+            return 240;
+        }
+    }
+}
---
+++ StoreBaeltTicketLibraryTests/StoreBaeltCarTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreBaeltTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary.Tests
+{
+    [TestClass()]
+    public class StoreBaeltCarTests
+    {
+        [TestMethod()]
+        public void PriceBrobizzTest()
+        {
+            var car = new StoreBaeltCar("123", DateTime.Now, true);
+            var output = car.Price();
+            Assert.AreEqual(228, output);
+        }
+        [TestMethod()]
+        public void PriceNoBrobizzTest()
+        {
+            var car = new StoreBaeltCar("123", DateTime.Now, false);
+            var output = car.Price();
+            Assert.AreEqual(240, output);
+        }
+        [TestMethod()]
+        public void PriceWeekendTest()
+        {
+            var car = new StoreBaeltCar("123", DateTime.Parse("26-09-2021"), false);
+            var output = car.Price();
+            Assert.AreEqual(192, output);
+        }
+        [TestMethod()]
+        public void PriceNotWeekendTest()
+        {
+            var car = new StoreBaeltCar("123", DateTime.Parse("27-09-2021"), false);
+            var output = car.Price();
+            Assert.AreEqual(240, output);
+        }
+        [TestMethod()]
+        public void PriceWeekendBrobizzTest()
+        {
+            var car = new StoreBaeltCar("123", DateTime.Parse("26-09-2021"), true);
+            var output = car.Price();
+            Assert.AreEqual(182.4, output);
+        }
+    }
+}
